2f12c07d03493171e643ae875cc20991aebe0866
Hudoon <hadoon2008@gmail.com>
2022-02-22 23:27:52 +0200

    changed

--- src/main/java/com/nulpointerexception/npechatroom/ChatRoomController.java
+++ src/main/java/com/nulpointerexception/npechatroom/ChatRoomController.java
@@ -10,33 +10,65 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import static java.lang.String.format;
 
 @Controller
+@CrossOrigin
 public class ChatRoomController {
 
     private static final Logger logger = LoggerFactory.getLogger(ChatRoomController.class);
     @Autowired
     private SimpMessageSendingOperations messagingTemplate;
+    //   private User user = null;
+
+
 
     @MessageMapping("/chat/{roomId}/sendMessage")
+    @SendTo("/topic/public")
     public void sendMessage(@DestinationVariable String roomId, @Payload Message chatMessage) {
         logger.info(roomId+" Chat messahe recieved is "+chatMessage.getContent());
         messagingTemplate.convertAndSend(format("/chat-room/%s", roomId), chatMessage);
     }
 
     @MessageMapping("/chat/{roomId}/addUser")
+    @SendTo("/topic/public")
     public void addUser(@DestinationVariable String roomId, @Payload Message chatMessage,
-                        SimpMessageHeaderAccessor headerAccessor) {
-        String currentRoomId = (String) headerAccessor.getSessionAttributes().put("room_id", roomId);
-        if (currentRoomId != null) {
-            Message leaveMessage = new Message();
-            leaveMessage.setType(Message.MessageType.LEAVE);
-            leaveMessage.setSender(chatMessage.getSender());
-            messagingTemplate.convertAndSend(format("/chat-room/%s", currentRoomId), leaveMessage);
-        }
-        headerAccessor.getSessionAttributes().put("name", chatMessage.getSender());
+                        SimpMessageHeaderAccessor headerAccessor) throws Exception {
+        headerAccessor.getSessionAttributes().put("username",chatMessage.getSender());
+
+
         messagingTemplate.convertAndSend(format("/chat-room/%s", roomId), chatMessage);
+
     }
+
+    @MessageMapping("/chat/{roomId}/leaveUser")
+    @SendTo("/topic/public")
+    public void leaveUser(@DestinationVariable String roomId, @Payload Message chatMessage,
+                          SimpMessageHeaderAccessor headerAccessor) throws Exception {
+        logger.info("user leave"+chatMessage.getSender());
+        messagingTemplate.convertAndSend(format("/chat-room/%s", roomId), chatMessage);
+
+    }
+
+
+
+//    @MessageMapping("chat/user/{username}")
+//    @SendTo("/topic/public")
+//    public void showUsers(@DestinationVariable String username){
+//       logger.info("Getting " + username);
+//        List<User> users = new ArrayList<>();
+//        if (username!=null) {
+//            User user = new User();
+//            user.setName(username);
+//            users.add(user);
+//
+//        }
+//        //return users;
+//        users.forEach(user1 -> logger.info(user1.getName()));
+//
+//        messagingTemplate.convertAndSend("/chat-room/users", users);
+//    }
+
 }
